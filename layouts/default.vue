9755e0e21065e27660a0fb39c83e8c8afb1a740a
Techit Sumo Thawiang <68545084+techitwinner@users.noreply.github.com>
2024-08-31 22:38:02 +0700

    Update default.vue
    Fix CSS and remove NavigationHead

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div class="fixed -z-[100] h-full w-full bg-slate-100 dark:bg-slate-950"></div>
-    <NavigationHead/>
     <NavigationBar/>
     <main class="flex flex-col bg-slate-100 dark:bg-slate-950 items-center">
       <div class="flex flex-col w-full">
@@ -24,6 +23,17 @@
   }
   #wrapper2 {
     @apply flex flex-col items-center gap-8 z-[1] max-w-[1024px] px-4 py-8 w-full;
+  }
+}
+* ::selection {
+  background-color: black !important;
+  color: white !important;
+}
+
+@media (prefers-color-scheme: dark) {
+  * ::selection {
+    background-color: white !important;
+    color: black !important;
   }
 }
 </style>
