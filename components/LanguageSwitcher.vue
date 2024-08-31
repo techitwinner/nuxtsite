92bb98831a573b3cfd504d1bc5aab8493bab25cb
Techit Sumo Thawiang <68545084+techitwinner@users.noreply.github.com>
2024-08-31 22:35:55 +0700

    Update LanguageSwitcher.vue
    Add support for German (Deutsch) !! YAY

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -24,14 +24,28 @@
     const newLocale = locale.value === 'en' ? 'th' : 'en'
     router.push(switchLocalePath(newLocale))
   }
+  const setLocale = (newLocale) => {
+    locale.value = newLocale;
+    router.push(switchLocalePath(newLocale))
+  };
 
   const buttonText = computed(() => locale.value === 'en' ? 'TH' : 'EN')
+  const localeList = [
+    [
+      {label: 'English (English)', click: () => {setLocale('en')} },
+      {label: 'German (Deutsch)', click: () => {setLocale('de')} },
+      {label: 'Thai (ภาษาไทย)', click: () => {setLocale('th')} }
+    ]
+  ];
 </script>
 
 <template>
-  <UTooltip :text="t('ui.tooltips.navigationBar.langSwitcher')">
-    <UButton :square="square ? 'true' : 'false'" variant="solid" color="white" size="xl" class="" @click="toggleLanguage">
-      <i class="ph ph-globe text-[24px]"></i><span v-if="displayLocaleValue">{{ buttonText }}</span>
-    </UButton>
-  </UTooltip>
+<!--  <UTooltip :text="t('ui.tooltips.navigationBar.langSwitcher')">-->
+<!--    <UButton :square="square ? 'true' : 'false'" variant="solid" color="white" size="xl" class="" @click="toggleLanguage">-->
+<!--      <i class="ph ph-globe text-[24px]"></i><span v-if="displayLocaleValue">{{ buttonText }}</span>-->
+<!--    </UButton>-->
+<!--  </UTooltip>-->
+  <UDropdown :items="localeList" :popper="{ placement: 'bottom-end' }">
+    <UButton square color="white" label="Locale" :color="locale.value === 'en' ? 'black' : 'white'" size="xl"><i class="text-[24px] ph ph-globe"></i></UButton>
+  </UDropdown>
 </template>
