a09c982f9f97b35dcb1e91cd14d25ab2d7b39fdb
Techit Sumo Thawiang <68545084+techitwinner@users.noreply.github.com>
2024-08-31 22:38:35 +0700

    Update nuxt.config.ts
    Updated,

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
+const strapiBaseUri = process.env.STRAPI_URL || "https://api.techit.win";
+
 export default defineNuxtConfig({
   css: ['./assets/css/main.css', './assets/css/font-faces.css'],
   compatibilityDate: '2024-04-03',
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@nuxtjs/i18n', 'nuxt-anchorscroll', 'nuxt-aos', '@nuxtjs/strapi', '@formkit/auto-animate'],
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      strapiUrl: process.env.STRAPI_URL || 'https://api.techit.win'
+    }
+  },
   i18n: {
     locales: [
       { code: 'en', iso: 'en-US', file: 'en.json' },
-      { code: 'th', iso: 'th-TH', file: 'th.json' },
+      { code: 'de', iso: 'de-DE', file: 'de.json' },
+      { code: 'th', iso: 'th-TH', file: 'th.json' }
     ],
     defaultLocale: 'en',
     strategy: 'prefix',
@@ -18,13 +27,12 @@ export default defineNuxtConfig({
       redirectOn: 'root',
     },
   },
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@nuxtjs/i18n', 'nuxt-anchorscroll'],
-  anchorscroll: {
-    hooks: [
-      // Or any valid hook if needed
-      // Default is `page:finish`
-      'page:transition:finish',
-    ],
+  strapi: {
+    url: strapiBaseUri,
+    prefix: '/api',
+    version: 'v4',
+    entities: ['nuxtsite-blog-posts'],
+    cookieName: 'strapi_jwt',
   },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
@@ -46,8 +54,10 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/vnd.microsoft.icon', href: '/favicon.ico' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'preconnect', href: 'https://rsms.me/'},
+        { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css'},
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'},
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&family=Bricolage+Grotesque:opsz,wdth,wght@12..96,75..100,200..800&family=Noto+Color+Emoji&display=swap' }
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100..900&family=Noto+Color+Emoji&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap' }
       ]
     }
   },
