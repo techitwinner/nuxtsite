8ad7addc6a0755b353a5de139b14d67a24b300a7
Techit Sumo Thawiang <68545084+techitwinner@users.noreply.github.com>
2024-08-31 22:38:13 +0700

    Create error.vue
    Layout for error.vue page

---
+++ layouts/error.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="fixed -z-[100] h-full w-full bg-slate-100 dark:bg-slate-950"></div>
+    <main class="flex flex-col bg-slate-100 dark:bg-slate-950 items-center">
+      <div class="flex flex-col w-full">
+        <slot/>
+      </div>
+    </main>
+  </div>
+</template>
+
+<style>
+.bg-err {
+  background-image: image('https://files.techit.win/images/background/ErrorScratch@2x.png');
+}
+
+#container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  #wrapper {
+    padding: 2rem 1rem;
+    max-width: 1024px;
+    width: 100%;
+  }
+  #wrapper2 {
+    @apply flex flex-col items-center gap-8 z-[1] max-w-[1024px] px-4 py-8 w-full;
+  }
+}
+</style>
+
+<script setup lang="ts">
+  import { ref, provide } from 'vue';
+
+  const drawerState = ref({ isOpen: false });
+
+  provide('drawerState', drawerState);
+</script>
