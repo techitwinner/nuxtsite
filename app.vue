6678fc72e03bff9b88b6b765b44a4c4d9888784a
Techit Sumo Thawiang <68545084+techitwinner@users.noreply.github.com>
2024-09-14 22:25:06 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,11 +1,29 @@
 <template>
   <Html>
-    <NuxtLoadingIndicator />
+    <NuxtLoadingIndicator color="white" class="hidden dark:block" />
+    <NuxtLoadingIndicator color="black" class="block dark:hidden" />
     <NuxtLayout>
       <NuxtPage/>
     </NuxtLayout>
   </Html>
 </template>
+
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'th'
+  },
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} | Techit Thawiang` : 'Techit Thawiang'
+  },
+  link: [
+    {
+      rel: 'icon',
+      href: '/techit-favicon.svg'
+    }
+  ]
+})
+</script>
 
 <style>
 .page-enter-active,
@@ -19,15 +37,29 @@
 .page-enter-from,
 .layout-enter-from {
   opacity: 0;
-  filter: blur(16px);
-  transform: translateY(32px);
 }
 .page-leave-to,
 .layout-leave-to {
   opacity: 0;
-  filter: blur(16px);
-  transform: translateY(-32px);
+}
+@media (prefers-reduced-motion: no-preference) and (min-width: 396px) {
+  .page-enter-active,
+  .layout-enter-active {
+    transition: all 0.45s cubic-bezier(var(--easeOut));
+  }
+  .page-leave-active,
+  .layout-leave-active {
+    transition: all 0.25s cubic-bezier(var(--easeIn));
+  }
+  .page-enter-from,
+  .layout-enter-from {
+    opacity: 0;
+    transform: translateY(16px);
+  }
+  .page-leave-to,
+  .layout-leave-to {
+    opacity: 0;
+    transform: translateY(-16px);
+  }
 }
 </style>
-<script setup lang="ts">
-</script>
