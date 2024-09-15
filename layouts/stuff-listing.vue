c16d49dd4fc66b7a423eed25eb1d03b9915c44b0
Techit Sumo Thawiang <68545084+techitwinner@users.noreply.github.com>
2024-09-15 11:46:13 +0700

    Create stuff-listing.vue
    Renamed from nothing.vue

---
+++ layouts/stuff-listing.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col top-0">
+    <slot/>
+  </div>
+  <LazyStuffFooter class="bottom-0"/>
+</template>
+
+<style scoped>
+
+</style>
