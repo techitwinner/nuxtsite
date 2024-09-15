668db1b7507f13bb91297843a58c78a7b24906af
Techit Sumo Thawiang <68545084+techitwinner@users.noreply.github.com>
2024-09-15 11:45:57 +0700

    Create StuffFooter.vue
    Stuff footer

---
+++ components/Stuff/StuffFooter.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const currentYear = new Date().getFullYear()
+</script>
+
+<template>
+<div class="w-full flex flex-col font-mono text-sm items-center p-4 select-none">
+  <p>For file removal requests, please contact <strong>techit<span class="text-pink-300">[at]</span>dailitation<span class="text-orange-300">[dot]</span>xyz</strong>.</p>
+  <p>Files on this page not owned by me belong to their respective owners.</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
